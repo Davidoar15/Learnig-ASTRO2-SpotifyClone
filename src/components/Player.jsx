4188273978fa2247ccf89cde7c24bef097fc8a2b
Davidoar15 <davidoar15@gmail.com>
2023-12-21 12:55:49 -0300

    add/volume

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import { usePlayerStore } from "../store/playerStore"
+import { Slider } from "./Slider"
 
 export const Pause = () => (
     <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
@@ -9,11 +10,34 @@ export const Play = () => (
     <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>
 )
 
+const CurrentSong = ({ image, title, artists }) => {
+    return (
+        <div className="flex items-center gap-5 relative overflow-hidden">
+            <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden">
+                <img 
+                    src={image}
+                    alt={title}
+                />
+            </picture>
+
+            <div className="flex flex-col">
+                <h3 className="font-semibold text-sm block">
+                    {title}
+                </h3>
+                <span className="text-xs opacity-80">
+                    {artists?.join(', ')}
+                </span>
+            </div>
+        </div>
+    )
+}
+
 export function Player() {
 
-    const { isPlaying, setIsPlaying, currentMusic, setCurrentMusic } = usePlayerStore(state => state)
+    const { isPlaying, setIsPlaying, currentMusic } = usePlayerStore(state => state)
 
     const audioRef = useRef();
+    const volumeRef = useRef(1);
 
     useEffect(() => {
         isPlaying 
@@ -26,6 +50,7 @@ export function Player() {
         if (song) {
             const src = `/music/${playlist.id}/0${song.id}.mp3`;
             audioRef.current.src = src;
+            audioRef.current.volume = volumeRef.current
             audioRef.current.play()
         }
     }, [currentMusic]);
@@ -37,7 +62,7 @@ export function Player() {
     return (
         <div className="flex flex-row justify-between w-full px-4 z-50">
             <div>
-                Current Song...
+                <CurrentSong { ...currentMusic.song } />
             </div>
 
             <div className="grid place-content-center gap-4 flex-1">
@@ -50,14 +75,25 @@ export function Player() {
                             isPlaying ? <Pause /> : <Play /> 
                         }
                     </button>
+
+                    <audio ref={audioRef} />
                 </div>
             </div>
 
             <div className="grid place-content-center">
-                
+                <Slider 
+                    defaultValue={[100]}
+                    max={100}
+                    min={0}
+                    onValueChange={(value) => {
+                        const [newVolume] = value
+                        const volumeValue = newVolume / 100
+                        volumeRef.current = volumeValue
+                        audioRef.current.volume = volumeValue
+                    }}
+                    className="w-[95px]"
+                />
             </div>
-
-            <audio ref={audioRef} />
         </div>
     )
 }
